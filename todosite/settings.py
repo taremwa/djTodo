b20afba25f260269db01231921916b78734dfcd2
taremwa <marktaremwa92@gmail.com>
2024-06-25 23:21:53 +0300

    created a gitignore and added necessary files to it

--- todosite/settings.py
+++ todosite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vuo3)96z7#o62z876c4_7&cwu1@wx&65nyp09ay#+w)mv4fo16'
+SECRET_KEY = os.environ.get('SECRET_KEY',)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
