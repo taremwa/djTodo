1e050b82a1c060cc440c051065239706f5cc6d40
taremwa <marktaremwa92@gmail.com>
2024-06-27 18:15:04 +0300

    create-todo html added to main urls page to authenticate login & register links

--- todo/views.py
+++ todo/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'todo/index.html')
+
+def create_todo(request):
+    return render(request, 'todo/create-todo.html')
