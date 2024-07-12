e768dc47658e1f8e4430d7e9b126844ce477614d
taremwa <marktaremwa92@gmail.com>
2024-07-12 14:50:42 +0300

    using views to handle registration validation errors

--- authentication/views.py
+++ authentication/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.contrib import messages
 from validate_email import validate_email
+from .models import User
 
 # Create your views here.
 def register(request):
     if request.method == "POST":
-        context = {'has_error': False}
+        context = {'has_error': False, 'data': request.POST}
         email = request.POST.get('email')
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -13,13 +14,45 @@ def register(request):
 
 
         if len(password) < 6:
-            messages.add_message(request,messages.ERROR,'Password should atleast exceed 6 characters')
+            messages.add_message(request,messages.ERROR,
+                                 'Password should atleast exceed 6 characters')
             context['has_error'] = True
         
         if password != password2:
-            messages.add_message(request,messages.ERROR,'Password mismatch')
+            messages.add_message(request,messages.ERROR,
+                                 'Password mismatch')
             context['has_error'] = True
 
+        if not validate_email(email):
+            messages.add_message(request,messages.ERROR,
+                                 'Enter a valid email address')
+            context['has_error'] = True
+
+        if not username:
+            messages.add_message(request,messages.ERROR,
+                                 'Username is required')
+            context['has_error'] = True
+
+        if User.objects.filter(username=username).exists():
+            messages.add_message(request,messages.ERROR,
+                                 'Username is already taken, Choose another')
+            context['has_error'] = True
+
+        if User.objects.filter(email=email).exists():
+            messages.add_message(request,messages.ERROR,
+                                 'Email is already taken, Choose another')
+            context['has_error'] = True
+
+        if context['has_error']:
+            return render(request, 'authentication/register.html')
+        
+
+        user = User.objects.create_user(username=username, email=email)
+        user.set_password(password)
+        user.save()
+
+
+        messages.add_message(request, messages.SUCCESS,'Account Sucessfully created!, you can login')
 
 
     return render(request, 'authentication/register.html')
